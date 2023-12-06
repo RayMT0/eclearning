dc2ba6be1c1acbcaee62e03b8089276beb10c8dc
RayMT0 <raymarvin989@gmail.com>
2023-12-06 18:52:39 +0700

    add-footer

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -112,8 +112,45 @@
       </div>
 
       {{-- Footer --}}
-      <footer class="container">
-        
+      <footer class="container-lg d-flex flex-lg-row flex-column justify-content-between align-items-lg-start align-items-center text-lg-start text-center row-gap-4 p-5">
+        {{-- Footer Social Media --}}
+        <div class="foot_media d-flex flex-column">
+          <div class="brand d-flex flex-row mb-3 justify-content-lg-start justify-content-center">
+            <img src="/assets/icons/logo_EClearning.svg" alt="logo">
+            <p class="fs-4 fw-semibold d-flex mb-0">ECLearning</p>
+          </div>
+          <p class="fs-6 fw-medium">Follow on Social Media</p>
+          <div class="d-flex flex-row justify-content-lg-start justify-content-center column-gap-3">
+            <a href="" class="text-dark"><i class="bi bi-facebook fs-3"></i></a>
+            <a href="" class="text-dark"><i class="bi bi-instagram fs-3"></i></a>
+            <a href="" class="text-dark"><i class="bi bi-twitter-x fs-3"></i></a>
+          </div>
+          <p class="fs-6 fw-light mt-3">Copyright 2023. All rights reserved.</p>
+        </div>
+
+        {{-- Footer Links --}}
+        <div class="foot_content d-flex flex-column">
+          <p class="fs-4 fw-semibold">Links</p>
+          <a href="/home" class="text-decoration-none mb-1">Home</a>
+          <a href="" class="text-decoration-none mb-1">Help center</a>
+          <a href="" class="text-decoration-none mb-1">Service</a>
+        </div>
+
+        {{-- Footer Resources --}}
+        <div class="foot_content d-flex flex-column">
+          <p class="fs-4 fw-semibold">Resources</p>
+          <a href="" class="text-decoration-none mb-1">About Us</a>
+          <a href="" class="text-decoration-none mb-1">Career</a>
+          <a href="" class="text-decoration-none mb-1">Legal Notice</a>
+        </div>
+
+        {{-- Footer Contacts --}}
+        <div class="foot_content d-flex flex-column">
+          <p class="fs-4 fw-semibold">Contacts</p>
+          <p class="mb-1"><i class="bi bi-crosshair me-2"></i>Apartemen Mediterania 2 Tower J 1Q</p>
+          <p class="mb-1"><i class="bi bi-envelope me-2"></i>eclearning@gmail.com</p>
+          <p class="mb-1"><i class="bi bi-telephone me-2"></i>+62-817-2234-2917</p>
+        </div>
       </footer>
 </body>
 </html>
