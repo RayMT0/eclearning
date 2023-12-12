514c6e5bc0d84afb1b22b1e70670b03da8394729
RayMT0 <raymarvin989@gmail.com>
2023-12-12 20:37:32 +0700

    'web route fix'

--- routes/web.php
+++ routes/web.php
@@ -3,16 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PageController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 //Get Routes
 Route::redirect('/', '/home');
 Route::get('/home', [PageController::class, 'homePage'])->name('home');
